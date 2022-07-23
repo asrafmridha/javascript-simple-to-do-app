91cfde67b046152dcf77af446751c951326a3d14
asraf-mridha <asrafmridha@gmail.com>
2022-07-23 20:32:09 +0600

    Input data store in variables in js

---
+++ app.js
@@ -0,0 +1,7 @@
+
+var _name= document.getElementById("name").value;
+var _id=document.getElementById("id").value;
+var _department=document.getElementById("department").value;
+var _gpa=document.getElementById("gpa").value;
+
+
