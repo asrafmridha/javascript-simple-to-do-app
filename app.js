73a92a64539328fc86749cb3f44818d7c05efd5c
asraf-mridha <asrafmridha@gmail.com>
2022-07-23 23:24:57 +0600

    Completed // do more later

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
-var _name= document.getElementById("name").value;
-var _id=document.getElementById("id").value;
-var _department=document.getElementById("department").value;
-var _gpa=document.getElementById("gpa").value;
+        var _name= document.getElementById("name").value;
+        var _id=document.getElementById("id").value;
+        var _department=document.getElementById("department").value;
+        var _gpa=document.getElementById("gpa").value;
 
  function validate() { 
 
@@ -19,6 +19,41 @@ var _gpa=document.getElementById("gpa").value;
       }
       else if(_gpa== ""){
         alert("please give gpa First!");
+      }
+      else{
+
+        var _tbody=document.getElementById("tbody");
+        
+        var _tr= document.createElement("tr");
+        var _td= document.createElement("td");
+        _tbody.appendChild(_tr);
+        _tr.appendChild(_td);
+
+        _td.innerText=_name;
+
+        // for id 
+
+       var _td1= document.createElement("td");
+       _tr.appendChild(_td1);
+       _td1.innerHTML=_id;
+
+    //    for Dept 
+
+    var _td2= document.createElement("td");
+       _tr.appendChild(_td2);
+       _td2.innerHTML=_department;
+
+    //    for gpa 
+
+    
+    var _td3= document.createElement("td");
+       _tr.appendChild(_td3);
+       _td3.innerHTML=_gpa;
+
+    
+     console.log(_name);
+
+
       }
   }
 
