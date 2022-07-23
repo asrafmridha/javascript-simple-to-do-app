bfe5f8aa2fb56365ce4a91cd82ce7f8da8effd19
asraf-mridha <asrafmridha@gmail.com>
2022-07-23 20:46:26 +0600

    Form validation by js

--- app.js
+++ app.js
@@ -4,4 +4,22 @@ var _id=document.getElementById("id").value;
 var _department=document.getElementById("department").value;
 var _gpa=document.getElementById("gpa").value;
 
+ function validate() { 
+
+    if(_name== ""){
+
+        alert("please give a name First!");
+      }
+      else if( _id==""){
+        alert("please give a Id First!");
+    
+      }
+      else if(_department== ""){
+        alert("please give a Department First!");
+      }
+      else if(_gpa== ""){
+        alert("please give gpa First!");
+      }
+  }
+
 
