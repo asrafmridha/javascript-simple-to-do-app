23db26ed26329cb3ce884cccd843b7c924cbcf1f
asraf-mridha <asrafmridha@gmail.com>
2023-09-01 21:48:29 +0600

    Issue Fixed

--- app.js
+++ app.js
@@ -1,62 +1,38 @@
+let Name = document.getElementById("name").value;
+var id = document.getElementById("id").value;
+var department = document.getElementById("department").value;
+var gpa = document.getElementById("gpa").value;
 
-        var _name= document.getElementById("name").value;
-        var _id=document.getElementById("id").value;
-        var _department=document.getElementById("department").value;
-        var _gpa=document.getElementById("gpa").value;
+function validate() {
+  if (Name == "") {
+    alert("please give a name First!");
+  } else if (id == "") {
+    alert("please give a Id First!");
+  } else if (department == "") {
+    alert("please give a Department First!");
+  } else if (gpa == "") {
+    alert("please give gpa First!");
+  } else {
+    var tbody = document.getElementById("tbody");
+    var tr = document.createElement("tr");
+    var td = document.createElement("td");
+    tbody.appendChild(tr);
+    tr.appendChild(td);
+    td.innerText = Name;
 
- function validate() { 
+    // for id
+    var td1 = document.createElement("td");
+    tr.appendChild(td1);
+    td1.innerHTML = id;
 
-    if(_name== ""){
+    //    for Dept
+    var td2 = document.createElement("td");
+    tr.appendChild(td2);
+    td2.innerHTML = department;
 
-        alert("please give a name First!");
-      }
-      else if( _id==""){
-        alert("please give a Id First!");
-    
-      }
-      else if(_department== ""){
-        alert("please give a Department First!");
-      }
-      else if(_gpa== ""){
-        alert("please give gpa First!");
-      }
-      else{
-
-        var _tbody=document.getElementById("tbody");
-        
-        var _tr= document.createElement("tr");
-        var _td= document.createElement("td");
-        _tbody.appendChild(_tr);
-        _tr.appendChild(_td);
-
-        _td.innerText=_name;
-
-        // for id 
-
-       var _td1= document.createElement("td");
-       _tr.appendChild(_td1);
-       _td1.innerHTML=_id;
-
-    //    for Dept 
-
-    var _td2= document.createElement("td");
-       _tr.appendChild(_td2);
-       _td2.innerHTML=_department;
-
-    //    for gpa 
-
-    
-    var _td3= document.createElement("td");
-       _tr.appendChild(_td3);
-       _td3.innerHTML=_gpa;
-
-    
-     
-
-
-      }
+    //    for gpa
+    var td3 = document.createElement("td");
+    tr.appendChild(td3);
+    td3.innerHTML = gpa;
   }
-
-
-
-
+}
